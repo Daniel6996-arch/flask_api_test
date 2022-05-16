f2039ca17f63ec79476310c8e82a0661411a2609
Daniel6996-arch <dkariukinyutu@gmail.com>
2022-05-16 12:17:16 +0300

    app to test api enpoints with GET and POST methods

---
+++ db.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+conn = sqlite3.connect("dante.db")
+
+cursor = conn.cursor()
+sql_query = """CREATE TABLE book (
+    id INT PRIMARY KEY AUTOINCREMENT,
+    author TEXT NOT NULL,
+    title TEXT NOT NULL
+)
+"""
+
+cursor.execute(sql_query)
---
+++ test.py
@@ -0,0 +1,37 @@
+import sqlite3
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+
+def db_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect("dante.db")
+    except sqlite3.error as e:
+        print(e)
+    return conn        
+
+@app.route("/books", methods=["GET", "POST"])    
+def books():
+    conn = db_connection()
+    cursor = conn.cursor()
+
+    if request.method == "GET":
+        cursor = conn.execute("SELECT * FROM book")
+        books=[
+            dict(id=row[0],author=row[1],title=row[2])
+            for row in cursor.fetchall()
+        ]
+        if books is not None:
+            return jsonify(books)
+
+    if request.method == "POST":
+         new_author = request.form["author"]
+         new_title = request.form["title"]
+         sql = """INSERT INTO book (author, title) VALUES (?,?)"""
+         cursor = cursor.execute(sql, (new_author, new_title))
+         conn.commit()
+
+         return f"Book with id: {cursor.lastrowid} created successfully"
+
